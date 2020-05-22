9d563123022181b100f1e2737d5f123e15659dcb
Xu Zhaowei <paldier@hotmail.com>
2020-05-22 13:01:30 +0800

    repack swap

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -61,10 +61,10 @@
             "version": "1.0"
         }, 
         {
-            "build_date": "2020-05-15_13:10:13", 
+            "build_date": "2020-05-22_13:01:20", 
             "description": "老板，来一斤虚拟内存~", 
             "home_url": "Module_swap.asp", 
-            "md5": "c3676fcd5b899f701f9ca1a552676bd9", 
+            "md5": "9a1aef4b164a85b46f939b6a2f77c3b5", 
             "name": "swap", 
             "tar_url": "swap/swap.tar.gz", 
             "title": "虚拟内存", 
--- swap/config.json.js
+++ swap/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"2.3",
-"md5":"c3676fcd5b899f701f9ca1a552676bd9",
+"md5":"9a1aef4b164a85b46f939b6a2f77c3b5",
 "home_url":"Module_swap.asp",
 "title":"虚拟内存",
 "description":"老板，来一斤虚拟内存~",
-"build_date":"2020-05-15_13:10:13"
+"build_date":"2020-05-22_13:01:20"
 }
