bd45fc6cebd361ff9b9e61ff13d9da9df67bc94f
Xu Zhaowei <paldier@hotmail.com>
2020-05-22 13:23:24 +0800

    add aria2 unblockmusic

---
+++ aria2/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"2.8",
+"md5":"77c72e26959cb61000256b989f23064f",
+"home_url":"Module_aria2.asp",
+"title":"Aria2",
+"description":"linux下载利器",
+"build_date":"2020-05-21_22:15:43"
+}
---
+++ unblockmusic/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"1.0.2",
+"md5":"b0a79796f3c1778753a0a50da0c9fa92",
+"home_url":"Module_unblockmusic.asp",
+"title":"解锁网易云灰色歌曲",
+"description":"解锁网易云灰色歌曲",
+"build_date":"2020-05-22_13:02:41"
+}
