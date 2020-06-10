81c232b18525afb18e634ac6ceff09f294f9aff1
Xu Zhaowei <paldier@hotmail.com>
2020-06-10 11:10:00 +0800

    add frps

---
+++ frps/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"1.5",
+"md5":"9b62ac99f88ffd9adaa3fbc5f7179829",
+"home_url":"Module_frps.asp",
+"title":"frps穿透服务器",
+"description":"内网穿透利器，谁用谁知道。",
+"build_date":"2020-06-06_15:04:47"
+}
