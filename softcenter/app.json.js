51b22341e7dd541458c70ffd1a56725594f42f5d
Xu Zhaowei <paldier@hotmail.com>
2021-01-24 11:53:29 +0800

    fix zerotier

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -201,6 +201,16 @@
             "version": "1.7"
         }, 
         {
+            "build_date": "2021-01-24_11:53:26", 
+            "description": "分布式的虚拟以太网", 
+            "home_url": "Module_zerotier.asp", 
+            "md5": "2a2659d8854f3d3c22f7230ccf58ab04", 
+            "name": "zerotier", 
+            "tar_url": "zerotier/zerotier.tar.gz", 
+            "title": "zerotier", 
+            "version": "0.3"
+        }, 
+        {
             "build_date": "2020-12-02_14:14:04", 
             "description": "百度盘", 
             "home_url": "Module_baidupcs.asp", 
--- zerotier/config.json.js
+++ zerotier/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"0.2",
-"md5":"23345b9694c405e1224dadf0f4b23cd0",
+"version":"0.3",
+"md5":"2a2659d8854f3d3c22f7230ccf58ab04",
 "home_url":"Module_zerotier.asp",
 "title":"zerotier",
 "description":"分布式的虚拟以太网",
-"build_date":"2021-01-15_11:47:34"
+"build_date":"2021-01-24_11:53:26"
 }
