3d627f2ec14fa1467ec67b5f1f15f8cceb1abcfe
Xu Zhaowei <paldier@hotmail.com>
2021-02-01 16:18:22 +0800

    repack softcenter #2

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -242,7 +242,7 @@
         }
     ], 
     "home_url": "https://raw.githubusercontent.com/paldier/softcentermipsle/master", 
-    "md5": "d6602846d1c65edbd22d3160ce7f9d62", 
+    "md5": "232503b83dbdcbbf11e16a31107af261", 
     "tar_url": "softcenter/softcenter.tar.gz", 
     "version": "1.3.2"
 }
--- softcenter/config.json.js
+++ softcenter/config.json.js
@@ -1,4 +1,4 @@
 {
 "version":"1.3.2",
-"md5":"d6602846d1c65edbd22d3160ce7f9d62"
+"md5":"232503b83dbdcbbf11e16a31107af261"
 }
