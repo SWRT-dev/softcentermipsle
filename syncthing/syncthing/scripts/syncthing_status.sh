9eac511f28dc98051c2dd6c5bbc9698608625e19
CHN-MuXin <h@ph233.cn>
2021-05-07 18:49:09 +0800

    获取运行状态需要获取syncthing的版本

--- syncthing/syncthing/scripts/syncthing_status.sh
+++ syncthing/syncthing/scripts/syncthing_status.sh
@@ -2,7 +2,8 @@
 source /jffs/softcenter/scripts/base.sh
 
 syncthing_pid=`pidof syncthing`
-syncthing_version=`dbus get syncthing_version`
+#syncthing_version=`dbus get syncthing_version`
+syncthing_version=`/jffs/softcenter/bin/syncthing --version | head -n 1`
 if [ -n "$syncthing_pid" ];then
     echo "${syncthing_version} 进程运行正常！（PID：$syncthing_pid）" > /tmp/syncthing_status.log
 else
