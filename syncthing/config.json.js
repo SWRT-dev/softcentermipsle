a9af6f03bb4724b481f9e3be710d3ceff62e5c16
CHN-MuXin <h@ph233.cn>
2021-05-10 11:57:32 +0800

    版本显示插件版本

--- syncthing/config.json.js
+++ syncthing/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"0.3.1",
-"md5":"d4d49e79d6cace44a0454ce3a99e15ca",
+"md5":"9c35bada08bd424293b9552851d9de16",
 "home_url":"Module_syncthing.asp",
 "title":"syncthing",
 "description":"syncthing储存同步",
-"build_date":"2021-05-10_11:38:37"
+"build_date":"2021-05-10_11:48:21"
 }
--- syncthing/syncthing/scripts/syncthing_status.sh
+++ syncthing/syncthing/scripts/syncthing_status.sh
@@ -2,10 +2,10 @@
 source /jffs/softcenter/scripts/base.sh
 
 syncthing_pid=`pidof syncthing`
-#syncthing_version=`dbus get syncthing_version`
+syncthing_tool_version=`dbus get syncthing_version`
 syncthing_version=`/jffs/softcenter/bin/syncthing --version | head -n 1`
 if [ -n "$syncthing_pid" ];then
-    echo "${syncthing_version} 进程运行正常！（PID：$syncthing_pid）" > /tmp/syncthing_status.log
+    echo "当前插件版本：${syncthing_tool_version}；${syncthing_version} 进程运行正常！（PID：$syncthing_pid）" > /tmp/syncthing_status.log
 else
     echo "${syncthing_version} 进程未运行！" > /tmp/syncthing_status.log
 fi
