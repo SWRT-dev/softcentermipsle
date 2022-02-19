13a5ccee0dc8da9170940060a1a91475407cc96c
Dmitriy <dima3014@gmail.com>
2022-02-19 22:04:35 +0200

    Fixed mistake

--- softcenter/push_message_ru.json.js
+++ softcenter/push_message_ru.json.js
@@ -1,10 +1,10 @@
 {
 "title":"<em>Software Center&nbsp;для&nbsp;платформы&nbsp;mipsle</em>",
-"content1":"<b>Software Center: </b>1.4.1,<font color='#1E90FF'> Supports both SWRT and ASUSWRT mod firmware</font>",
-"content2":"New firmware 5.1.7 has been released",
+"content1":"<b>Software Center: </b>1.4.1,<font color='#1E90FF'> Поддерживает обе SWRT и ASUSWRT модифицированные прошивки</font>",
+"content2":"Вышла новая прошивка 5.1.7",
 "content3":"<font color='#1E90FF'>Обратная связь:&nbsp;&nbsp;</font>\
 <a href='https://github.com/paldier/softcentermipsle' target='_blank'><em>1.GitHub</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
-<a href='https://t.me/merlinchat' target='_blank'><em>2.Join telegram</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
-<a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.Chinese forum</em></a>",
+<a href='https://t.me/merlinchat' target='_blank'><em>2.Чат в Telegram</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
+<a href='https://www.right.com.cn/forum/forum-173-1.html' target='_blank'><em>3.Китайский форум</em></a>",
 "content4":"<font color='#1E90FF'>Версия прошивки:</font><b>"+db_softcenter_["softcenter_firmware_version"]+"</b>&nbsp;&nbsp;<font color='#1E90FF'>Последняя версия:</font><b>5.1.7</b>&nbsp;&nbsp;<a href='https://github.com/MerlinRdev/Supported_Devices/blob/master/Firmware_Download.md' target='_blank'><em>Скачать</em></a>"
 }
