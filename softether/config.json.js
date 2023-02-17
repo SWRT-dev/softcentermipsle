566c61e559216929c5862c015130378377753105
ZhaoWei Xu <paldier@hotmail.com>
2023-02-17 17:36:06 +0800

    upodate softether

--- softether/config.json.js
+++ softether/config.json.js
@@ -1,8 +1,9 @@
 {
-"version":"2.0",
-"md5":"efd595ec6d08f8d9ed563b1ff7bbf5b9",
+"version":"2.2",
+"md5":"1e35ad63f6eb4092c4cf00e582df053f",
+"arch":"mipsle",
 "home_url":"Module_softether.asp",
 "title":"SoftEther_VPN_Server",
 "description":"VPN全家桶, ver 4.30 build 9700",
-"build_date":"2020-08-20_12:26:30"
+"build_date":"2023-02-17_17:36:00"
 }
