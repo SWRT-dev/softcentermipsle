ca78137e6ae1510a374ecec50f5ce8fc4b408534
ZhaoWei Xu <paldier@hotmail.com>
2023-10-24 18:48:17 +0800

    update alist

--- alist/config.json.js
+++ alist/config.json.js
@@ -1,9 +1,9 @@
 {
-"version":"1.1.0",
-"md5":"3f9ef975a13f8d5f69469d64eb2afcd8",
+"version":"1.9.1",
+"md5":"d213a756e79526f5ed32aeed638c4488",
 "arch":"mipsle",
 "home_url":"Module_alist.asp",
 "title":"Alist文件列表",
 "description":"一款支持多种存储的目录文件列表程序，支持 web 浏览与 webdav，后端基于gin，前端使用react。",
-"build_date":"2023-02-18_16:08:38"
+"build_date":"2023-10-24_18:48:14"
 }
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -289,14 +289,14 @@
         }, 
         {
             "arch": "mipsle", 
-            "build_date": "2023-02-18_16:08:38", 
+            "build_date": "2023-10-24_18:48:14", 
             "description": "一款支持多种存储的目录文件列表程序，支持 web 浏览与 webdav，后端基于gin，前端使用react。", 
             "home_url": "Module_alist.asp", 
-            "md5": "3f9ef975a13f8d5f69469d64eb2afcd8", 
+            "md5": "d213a756e79526f5ed32aeed638c4488", 
             "name": "alist", 
             "tar_url": "alist/alist.tar.gz", 
             "title": "Alist文件列表", 
-            "version": "1.1.0"
+            "version": "1.9.1"
         }, 
         {
             "arch": "mipsle", 
